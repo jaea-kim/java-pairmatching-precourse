5ceb1f424b8b64045962ae8141f9c493b47db540
jaea-kim <jaeakim98@gmail.com>
2023-11-27 15:38:25 +0900

    FEAT: .md 파일 읽어 크루 정보 저장

---
+++ src/main/java/pairmatching/domain/Course.java
@@ -0,0 +1,21 @@
+package pairmatching.domain;
+
+public enum Course {
+    BACKEND("백엔드", "src/main/resources/backend-crew.md"),
+    FRONTEND("프론트엔드", "src/main/resources/frontend-crew.md");
+
+    private String name;
+    private String filePath;
+    Course(String name, String filePath) {
+        this.name = name;
+        this.filePath = filePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+}
---
+++ src/main/java/pairmatching/repository/CrewRepository.java
@@ -0,0 +1,38 @@
+package pairmatching.repository;
+
+import pairmatching.domain.Course;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CrewRepository {
+    private final String PAHT = "../resources";
+    private Map<Course, List<String>> crewRepository;
+
+    public void init() {
+        for (Course course : Course.values()) {
+            try {
+                List<String> crewNames = readCrewFile(course.getFilePath());
+                crewRepository.put(course, crewNames);
+            } catch (IOException e) {
+                throw new IllegalArgumentException("크루 정보를 읽어올 수 없습니다.");
+            }
+        }
+    }
+
+    public List<String> readCrewFile(String filePath) throws IOException {
+        List<String> crewList = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+            String line = "";
+
+            while ((line = br.readLine()) != null) {
+                crewList.add(line);
+            }
+        }
+        return crewList;
+    }
+
+}
